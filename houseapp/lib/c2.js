c3f4cc8b00f47ea7c5a830c7903916366bbe55fd
Martyn Adair <adair@mountainnews.com>
2016-10-22 18:51:25 -0700

    ctests

--- houseapp/lib/c2.js
+++ houseapp/lib/c2.js
@@ -13,7 +13,17 @@ Cylon.robot({
   devices: {
     singingvoice: { driver: 'relay', pin: 16, type: "closed" },
     playwithme: { driver: 'relay', pin: 17, type: "closed" },
-    led: { driver: 'relay', pin: 18, type: "closed" },
+    //led: { driver: 'relay', pin: 18, type: "closed" },
+    //
+    //
+    elwireVoice1: {drivers:'direct-pin', pin:2},
+    elwireVoice1: {drivers:'direct-pin', pin:3},
+    elwireVoice1: {drivers:'direct-pin', pin:4},
+    elwireVoice1: {drivers:'direct-pin', pin:5},
+    elwireVoice1: {drivers:'direct-pin', pin:6},
+    elwireVoice1: {drivers:'direct-pin', pin:7},
+    elwireVoice1: {drivers:'direct-pin', pin:8},
+    elwireVoice1: {drivers:'direct-pin', pin:9},    
   },
 
   work: function(my) {
